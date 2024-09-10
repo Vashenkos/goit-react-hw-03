99b200f6466972d52f33a925e8f62d6b74178ecb
Vashenkos <159846523+Vashenkos@users.noreply.github.com>
2024-09-10 23:22:05 +0300

    в

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,10 @@
+const Contact = ({ name, number }) => {  
+    return (  
+        <div className="contact">  
+            <p>Name: {name}</p>  
+            <p>Number: {number}</p>  
+        </div>  
+    );  
+};  
+
+export default Contact;  
