48549d2c34af127219afa2428e1b3e7fea3ba713
Vashenkos <159846523+Vashenkos@users.noreply.github.com>
2024-09-13 01:11:33 +0300

    1

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,10 +1,19 @@
-const ContactList = ({ name, number }) => {  
+const ContactList = ({ contacts }) => {  
     return (  
-        <div className="contact">  
-            <p>Name: {name}</p>  
-            <p>Number: {number}</p>  
+        <div className="contact-list">  
+            {contacts.map((contact) => ( 
+                <div className="contact"
+                     key={contact.id}> 
+                     <div className="pcon">
+                    
+                     <p> Name: {contact.name}</p>  
+                     <p> <img className="imgpng " src="/src/img/call.png "  /> {contact.number}</p> 
+                     </div>
+                    <button  className="buttonDelete"type="button">Delete</button>  
+                </div>  
+            ))}  
         </div>  
     );  
 };  
 
-export default ContactList;  
+export default ContactList;
--- src/img/er.jsx
+++
@@ -1,9 +0,0 @@
-import { FaBeer } from 'react-icons/fa';  
-
-const MyComponent = () => (  
-  <div>  
-    <h3>Let's go for a beer! <FaBeer /></h3>  
-  </div>  
-);  
-
-export default MyComponent;
